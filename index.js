6c23fcce4d32f1505f77ea9f4b2affdae0b71c20
Christian Wacker <christianwwacker@gmail.com>
2020-01-07 02:22:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // write your code below!
 function happyHolidays(string) {
-  return "Happy holidays!"
+  return `Happy holidays!`
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
 }
